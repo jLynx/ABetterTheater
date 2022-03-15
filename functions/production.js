e1bc244df6afa28b7f3f5a5d8015384ba4752503
jLynx <admin@jlynx.net>
2022-03-15 21:00:54 +1300

    Testing fucntions

---
+++ functions/production.js
@@ -0,0 +1,15 @@
+addEventListener("fetch", (event) => {
+  const data = {
+    country: event.request.headers.get("cf-ipcountry"),
+  };
+
+  const json = JSON.stringify(data);
+
+  return event.respondWith(
+    new Response(json, {
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+    })
+  );
+});
