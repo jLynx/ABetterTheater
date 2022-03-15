099dd60abbd366f33aee5393dd67463f1c8c711b
jLynx <admin@jlynx.net>
2022-03-15 21:22:13 +1300

    Testing fucntions

---
+++ functions/api/country.js
@@ -0,0 +1,22 @@
+export async function onRequestGet(request) {
+  const data = {
+    country: request.headers.get("cf-ipcountry"),
+  };
+
+  const json = JSON.stringify(data);
+
+  // return respondWith(
+  //   new Response(json, {
+  //     headers: {
+  //       "content-type": "application/json;charset=UTF-8",
+  //     },
+  //   })
+  // );
+
+
+  // const res = await fetch(`https://rickandmortyapi.com/api/character/${params.id}`);
+  // const data = await res.json();
+  // const info = JSON.stringify(data);
+
+  return new Response(json, null, 2);
+}
